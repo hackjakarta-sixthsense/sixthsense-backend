aaf908eff1c9dc2239cdcb366812298223b0ebcc
haryharsono <311098haryharsono@gmail.com>
2024-07-27 13:30:03 +0700

    [create] setup project

---
+++ src/main/java/sixthsense_backend/sixthsense_backend/SixthsenseBackendApplication.java
@@ -0,0 +1,13 @@
+package sixthsense_backend.sixthsense_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixthsenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixthsenseBackendApplication.class, args);
+	}
+
+}
