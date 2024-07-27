3cef0227dc18c6f7a0fd02acf477904a937471af
haryharsono <311098haryharsono@gmail.com>
2024-07-27 13:37:28 +0700

    [create] remove unusued

--- src/main/java/sixthsense/backend/SixthsenseBackendApplication.java
+++ src/main/java/sixthsense/backend/SixthsenseBackendApplication.java
@@ -1,4 +1,4 @@
-package sixthsense_backend.sixthsense_backend;
+package sixthsense.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
